61e69d0d868f437f4ed1adc6f18d48d88ccacda2
selwa-elfirdoussi <selwa.elfirdoussi@emines.um6p.ma>
2022-10-29 10:25:45 +0100

    create fxml file

---
+++ GestionBanque/src/gestionbanque/ClientbanqueController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionbanque;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author selwa.elfirdoussi
+ */
+public class ClientbanqueController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
